5e389fdc48090c4e31af97d954dccddc61e9c902
Key Yu Wan <keyflcbyuwan@gmail.com>
2023-03-06 19:50:28 -0300

    components Box and Text

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,20 @@
+import { StoryObj, Meta } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur dolorem delectus fugiat et aspernatur totam eveniet fuga minima corrupti eligendi modi consequuntur, exercitationem animi impedit. Voluptas quia explicabo laboriosam numquam!',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
