f1db21a44d4dc1ceafcb38562255148dda6e001d
Key Yu Wan <keyflcbyuwan@gmail.com>
2023-03-07 15:13:54 -0300

    TextInput & TextArea stories

---
+++ packages/docs/src/stories/TextArea.stories.tsx
@@ -0,0 +1,32 @@
+import { StoryObj, Meta } from '@storybook/react'
+import { Box, Text, TextArea, TextAreaProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Form/Text Area',
+  component: TextArea,
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{ display: 'flex', flexDirection: 'column', gap: '$2' }}
+        >
+          <Text size="sm">Description</Text>
+          {Story()}
+        </Box>
+      )
+    },
+  ],
+} as Meta<TextAreaProps>
+
+export const Primary: StoryObj<TextAreaProps> = {
+  args: {
+    placeholder: 'Describe yourself',
+  },
+}
+
+export const Disabled: StoryObj<TextAreaProps> = {
+  args: {
+    disabled: true,
+  },
+}
